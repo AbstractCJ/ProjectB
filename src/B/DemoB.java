3e8662b4e87c69ef1bae5639f9b4c1954c59d269
User <User@DESKTOP-6JPNDEN.lan>
2024-01-03 21:10:38 -0500

    DemoB

---
+++ src/B/DemoB.java
@@ -0,0 +1,15 @@
+package B;
+
+public class DemoB {
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("Git");
+		System.out.println("Day 1");
+		System.out.println("1/3/2024");
+		
+	}
+
+
+}
